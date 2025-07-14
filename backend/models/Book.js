abd868bc781979882ae6076669fe2f1369a7c546
ShulavShakya <shakyashulav29@gmail.com>
2025-07-14 22:04:13 +0545

    Added partial fields

---
+++ backend/models/Book.js
@@ -0,0 +1,29 @@
+import { mongoose } from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isbn: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  available: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const Book = mongoose.model("Book", bookSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import { mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["borrower", "librarian"],
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
