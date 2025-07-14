efdde1577c6a52082d74c48e9103b691adf8079d
ShulavShakya <shakyashulav29@gmail.com>
2025-07-15 04:24:24 +0545

    Implemented the rate limiter

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { login } from "../controller/authController.js";
+import { limiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", limiter, login);
 
 export default router;
