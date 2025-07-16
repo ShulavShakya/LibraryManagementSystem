3a97590c806eab300e12b788f1ab178df1152b19
ShulavShakya <shakyashulav29@gmail.com>
2025-07-17 00:21:04 +0545

    added middleware

--- backend/src/routes/bookRoute.js
+++ backend/src/routes/bookRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { createBook } from "../controller/bookController.js";
+import {
+  addNewBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  removeBook,
+} from "../controller/bookController.js";
 
 const router = express.Router();
 
-router.post("/", createBook);
+router.post("/", [authenticateToken, checkRole("librarian")], addNewBook);
+router.get("/", [authenticateToken, checkRole("librarian")], getAllBooks);
+router.get("/:id", [authenticateToken, checkRole("librarian")], getBookById);
+router.delete("/:id", [authenticateToken, checkRole("librarian")], removeBook);
+router.put("/:id", [authenticateToken, checkRole("librarian")], updateBook);
 
 export default router;
