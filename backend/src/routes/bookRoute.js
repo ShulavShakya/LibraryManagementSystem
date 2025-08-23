6753a0fb4a801f58fa6c7ada33064fafe9fd5270
ShulavShakya <shakyashulav29@gmail.com>
2025-08-23 09:40:56 +0545

    updated the endpoints

--- backend/src/routes/bookRoute.js
+++ backend/src/routes/bookRoute.js
@@ -10,10 +10,18 @@ import { authenticateToken, checkRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", [authenticateToken, checkRole("librarian")], addNewBook);
-router.get("/", [authenticateToken], getAllBooks);
-router.get("/:id", [authenticateToken], getBookById);
-router.delete("/:id", [authenticateToken, checkRole("librarian")], removeBook);
-router.put("/:id", [authenticateToken, checkRole("librarian")], updateBook);
+router.post("/add", [authenticateToken, checkRole("librarian")], addNewBook);
+router.get("/get", [authenticateToken], getAllBooks);
+router.get("/get/:id", [authenticateToken], getBookById);
+router.delete(
+  "/delete/:id",
+  [authenticateToken, checkRole("librarian")],
+  removeBook
+);
+router.put(
+  "/update/:id",
+  [authenticateToken, checkRole("librarian")],
+  updateBook
+);
 
 export default router;
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -11,9 +11,21 @@ import { authenticateToken, checkRole } from "../middleware/authMiddleware.js";
 const Router = express.Router();
 
 Router.post("/register", registerUser);
-Router.get("/", [authenticateToken, checkRole("librarian")], getUsers);
-Router.get("/:id", [authenticateToken, checkRole("librarian")], getUserById);
-Router.delete("/:id", [authenticateToken, checkRole("librarian")], deleteUser);
-Router.put("/:id", [authenticateToken, checkRole("librarian")], updateUser);
+Router.get("/get", [authenticateToken, checkRole("librarian")], getUsers);
+Router.get(
+  "/get/:id",
+  [authenticateToken, checkRole("librarian")],
+  getUserById
+);
+Router.delete(
+  "/delete/:id",
+  [authenticateToken, checkRole("librarian")],
+  deleteUser
+);
+Router.put(
+  "/update/:id",
+  [authenticateToken, checkRole("librarian")],
+  updateUser
+);
 
 export default Router;
