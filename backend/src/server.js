4f85088c03eedb51d9a80e0e94812e6a1712c833
ShulavShakya <shakyashulav29@gmail.com>
2025-08-30 22:19:32 +0545

    Change

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://the-whimsical.vercel.app/",
+    origin: "https://the-whimsical.vercel.app/" || "*",
     credentials: true,
   })
 );
