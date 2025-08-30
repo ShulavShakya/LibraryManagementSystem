167539974926b5c8370a64978a94e9d566d443e6
ShulavShakya <shakyashulav29@gmail.com>
2025-08-30 20:50:06 +0545

    Updated the cors url

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://the-whimsical.vercel.app/",
     credentials: true,
   })
 );
