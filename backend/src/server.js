c7140ae18d757cc4b939357a56540791a562a385
ShulavShakya <shakyashulav29@gmail.com>
2025-08-31 10:43:31 +0545

    Updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,34 +12,39 @@ dotenv.config();
 
 //declaration of apps and port
 const app = express();
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 const port = process.env.PORT || 5050;
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow requests like Postman/cURL
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true); // allow requests like Postman/cURL
 
-    const allowedOrigins = [
-      "http://localhost:5050", // local dev
-      "https://the-whimsical.vercel.app", // production
-    ];
+//     const allowedOrigins = [
+//       "http://localhost:5050", // local dev
+//       "https://the-whimsical.vercel.app", // production
+//     ];
 
-    // Allow any vercel preview subdomain for this project
-    const vercelPreviewRegex =
-      /^https:\/\/the-whimsical-[\w-]+-shulavshakyas-projects\.vercel\.app$/;
+//     // Allow any vercel preview subdomain for this project
+//     const vercelPreviewRegex =
+//       /^https:\/\/the-whimsical-[\w-]+-shulavshakyas-projects\.vercel\.app$/;
 
-    if (allowedOrigins.includes(origin) || vercelPreviewRegex.test(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
+//     if (allowedOrigins.includes(origin) || vercelPreviewRegex.test(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
 
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:8081"],
+    credentials: true,
+  })
+);
 
 //Route
 app.use("/api", routes);
